6e18a4a61b522e179707a4337b6ee8724d4466b1
Rafal <rafal.robi.nowak@gmail.com>
2023-02-01 13:51:27 +0100

    przyklad zapytania tworzacego tabele

--- websites/default/public/index.php
+++ websites/default/public/index.php
@@ -1,10 +1,19 @@
 <?php
 try {
     $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4','ijdbuser', 'tajemnica');
-    $output = "Udane połaczenie z bazą danych.";
+    
+    $sql = 'CREATE TABLE joke (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        joketext TEXT,
+        jokedate DATE NOT NULL
+        ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
+    $pdo->exec($sql);
+
+    $output = 'Tabela "joke" została utworzona.'; 
 } catch (PDOException $e) {
-    $output = "Nie udało się połączyć z bazą danych. <br/>";
-    $output .= $e;
+    $output = "Błąd bazy danych: ";
+    $output .= $e->getMessage().' w pliku: '.$e->getFile().' w linii: ';
+    $output .= $e->getLine();
 }
 
 include __DIR__.'/../templates/output.html.php';
