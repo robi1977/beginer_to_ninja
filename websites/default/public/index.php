ff49b212c1c39a4a88c1225cae2ed1f4cbae5003
Rafal <rafal.robi.nowak@gmail.com>
2023-02-01 13:20:41 +0100

    dodanie kodowania znakow

--- websites/default/public/index.php
+++ websites/default/public/index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=mysql;dbname=ijdb','ijdbuser', 'tajemnica');
+    $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4','ijdbuser', 'tajemnica');
     $output = "Udane połaczenie z bazą danych.";
 } catch (PDOException $e) {
     $output = "Nie udało się połączyć z bazą danych. <br/>";
