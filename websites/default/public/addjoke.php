885a19aa1c85a7a6c9936fff48c981613eac82d6
Rafal <rafal.robi.nowak@gmail.com>
2023-02-13 15:25:03 +0100

    widok i obsluga dodawania dowcipow

---
+++ websites/default/public/addjoke.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_POST['joketext']) && strlen($_POST['joketext'])>0) { 
+    try {
+        $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4','ijdbuser', 'tajemnica');
+        //przygotowania zapytania do bazy
+        $sql = 'INSERT INTO `joke` SET `joketext` = :joketext, `jokedate` = CURDATE()';
+        $stsm = $pdo->prepare($sql);
+        //powiązanie zmiennych z zapytania ze zmiennymi z tablicy $_POST
+        $stsm->bindValue(':joketext', $_POST['joketext']);
+        //wykonanie zapytania
+        $stsm->execute();
+
+        //przekierowanie na stronę z listą dowcipów
+        header('location: jokes.php');
+
+    } catch (PDOException $e) {
+        $title = 'Wystąpił błąd.';
+        $output = "Błąd bazy danych ".$e->getMessage().' w pliku: '.$e->getFile().' w linii: '.$e->getLine();
+    }
+} else {
+    $title = "Dodaj nowy dowcip.";
+    ob_start();
+    include __DIR__.'/../templates/addjoke.html.php';
+    $output = ob_get_clean();
+}
+
+include __DIR__.'/../templates/layout.html.php';
---
+++ websites/default/templates/addjoke.html.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+    <label for="joketext">Wpisz tekst Twojego dowcipu:</label>
+    <textarea id="joketext" name="joketext" rows="3" cols="40"></textarea>
+    <input type="submit" name="submit" value="Dodaj"/>
+</form>
