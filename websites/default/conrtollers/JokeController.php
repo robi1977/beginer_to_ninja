301fbb43e27e334fbbde5f33b803b11a7341081e
Rafal <rafal.robi.nowak@gmail.com>
2023-02-16 15:44:27 +0100

    dodanie metod do kontrolera

--- websites/default/conrtollers/JokeController.php
+++ websites/default/conrtollers/JokeController.php
@@ -7,5 +7,74 @@ class JokeController
 
     }
 
+    public function list()
+    {
+        $result = $this->jokesTable->findAll();
 
+        $jokes = [];
+        foreach($result as $joke) {
+            $author = $this->authorsTable->find('id', $joke['authorid'])[0];
+
+            $jokes = [
+                'id' => $joke['id'],
+                'joketext' => $joke['joketext'],
+                'jokedate' => $joke['jokedate'],
+                'name' => $author['name'],
+                'email' => $author['email']
+            ];
+        }
+        $title = "Lista dowcipów";
+        $totalJokes = $this->jokesTables->total();
+
+        ob_start(); //otwarcie bufora
+        include __DIR__.'/../templates/jokes.html.php'; //wczytanie konstrukcji html z pliku tworzącego listę dowcipów
+        $output = ob_get_clean(); //przeniesienie bufora do zmiennej i wyczyszczenie go
+
+        return ['output'=>$output, 'title'=>$title];
+    }
+
+    public function home()
+    {
+        $title = 'Internetowa baza dowcipów';
+        ob_start();
+        include __DIR__.'/../templates/home.html.php';
+        $output = ob_get_clean();
+
+        return ['output'=>$output, 'title'=>$title];
+    }
+
+    public function delete()
+    {
+        $this->jokesTable->delete('id', $_POST['id']);
+
+        header('location: jokes.php');
+    }
+
+    public function edit()
+    {
+        if (isset($_POST['joke'])) {
+            $joke = $_POST['joke'];
+            $joke['jokedate'] = new DateTime();
+            $joke['authorId'] = 1;
+
+            $this->jokesTable->save($joke);
+
+            header('location: jokes.php');
+        } else {
+            if (isset($_GET['id'])) {
+                $joke = $this->jokesTable->find('id', $_GET['id'])[0] ?? null;
+
+            } else {
+                $joke = null;
+            }
+
+            $title = 'Edytuj dowcip';
+            
+            ob_start();
+                include __DIR__.'/../templates/editjoke.html.php';
+            $output = ob_get_clean();
+        }
+
+        return ['output'=>$output, 'title'=>$title];
+    }
 }
